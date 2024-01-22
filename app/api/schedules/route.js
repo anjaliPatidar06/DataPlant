ccd64c0a051025d0ff9be3bfef16cfe12a57ef40
anjaliPatidar06 <anjali.patidar@techsolvo.com>
2024-01-22 15:43:32 +0530

    update temp

--- app/api/schedules/route.js
+++ app/api/schedules/route.js
@@ -11,7 +11,8 @@ const searchScheduleByTitle = (fileData, title) => {
 
 export const GET = async (request) => {
     // const filePath = path.join('public', 'assets', 'data.json');
-    const filePath = path.join('tmp', 'data.json');
+    // const filePath = path.join('tmp', 'data.json');
+    const filePath = path.resolve(process.cwd(), 'tmp', 'data.json');
     if (!fs.existsSync(filePath)) {
         // If the file doesn't exist, create an empty one
         const emptyData = { schedules: [] };
@@ -38,7 +39,8 @@ export const GET = async (request) => {
 
 export const POST = async (request) => {
     // const filePath = path.join('public', 'assets', 'data.json');
-    const filePath = path.join('tmp', 'data.json');
+    // const filePath = path.join('tmp', 'data.json');
+    const filePath = path.resolve(process.cwd(), 'tmp', 'data.json');
     if (!fs.existsSync(filePath)) {
         // If the file doesn't exist, create an empty one
         const emptyData = { schedules: [] };
